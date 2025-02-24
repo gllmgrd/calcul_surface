87c2ae4c5ca3cdc6c721b1684c898bcb863901aa
gllmgrd <gir.guillaume@gmail.com>
2025-02-24 15:39:43 +0100

    tresh:254

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def remove_green_background(image, lower_bound, upper_bound):
 # Fonction pour détecter les contours
 def detect_contours(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(gray, 254, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     contour_image = np.ones_like(image) * 255  # Fond blanc
