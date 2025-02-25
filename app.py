35ea2e8c7298eee46a4bc430a201cfb812a1ee40
gllmgrd <gir.guillaume@gmail.com>
2025-02-25 15:51:41 +0100

    tresh 254

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def remove_green_background(image, lower_bound, upper_bound):
 
 def detect_contours(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(gray, 254, 255, cv2.THRESH_BINARY_INV)
 
     # Utilisation de cv2.RETR_TREE pour récupérer les contours intérieurs et extérieurs
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
